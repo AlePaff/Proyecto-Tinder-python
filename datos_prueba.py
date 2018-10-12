4fb635b76744b12590f09733b17a06de3138c1d4
AlePaff <43119891+AlePaff@users.noreply.github.com>
2018-10-12 19:06:21 -0300

    Add files via upload

---
+++ datos_prueba.py
@@ -0,0 +1,27 @@
+baseDeDatos={
+    "Juan89":[
+        "Juan",
+        "Rodriguez",
+        "juancitoR89",    #contraseña
+        "Hombre",
+        24,
+        41.40338,   #latitud
+        2.17403,    #longitud
+        ["tenis", "nadar", "pokemon", "anime", "escribir", "pintar", "taekwondo"]
+    ],
+    "LaJuancita":[
+        "Juana",
+        "Perez",
+        "redsun9887",
+        "Mujer""",
+        21,
+        42.34567,
+        1.26785,
+        ["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo","buenos-aires"]
+    ],
+}
+
+print(baseDeDatos["LaJuancita"])
+
+
+#si te da paja pensar buscá "lista de hobbies"
