fcf20813b98d02e911df12a868bf5e8ee667058c
AlePaff <43119891+AlePaff@users.noreply.github.com>
2018-10-24 16:08:32 -0300

    Update datos_prueba.py

--- datos_prueba.py
+++ datos_prueba.py
@@ -1,4 +1,4 @@
-dicDatos={
+dicDatos = {
     "juan": {
         "nombre": "Juan",
         "apellido": "Rodriguez",
@@ -10,44 +10,55 @@ dicDatos={
         "likes": {},
         "mensajes": {"lasd": 2},
     },
-    "LaJuancita":{
-        "nombre":"Juana",
-        "apellido":"Perez",
-        "contraseña":"redsun9887",
-        "sexo":"M",
-        "edad":21,
-        "ubicacion":[42.34567, 23.123],
-        "intereses":["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo","buenos-aires"],
+    "LaJuancita": {
+        "nombre": "Juana",
+        "apellido": "Perez",
+        "contraseña": "redsun9887",
+        "sexo": "M",
+        "edad": 21,
+        "ubicacion": [42.34567, 23.123],
+        "intereses": ["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo",
+                      "buenos-aires"],
     },
-	"usuario1":{
-        "nombre":"usuario1",
-        "apellido":"usuario1",
-        "contraseña":"usuario1",
-        "sexo":"I",
-        "edad":32,
-        "ubicacion":[2.34567, 23.123],
-        "intereses":["star-wars", "asado", "bicicleta", "taekwondo","buenos-aires"],
+    "usuario1": {
+        "nombre": "usuario1",
+        "apellido": "usuario1",
+        "contraseña": "usuario1",
+        "sexo": "I",
+        "edad": 32,
+        "ubicacion": [2.34567, 23.123],
+        "intereses": ["star-wars", "asado", "bicicleta", "taekwondo", "buenos-aires"],
     },
-	"usuario2":{
-        "nombre":"usuario2",
-        "apellido":"usuario2",
-        "contraseña":"usuario2",
-        "sexo":"H",
-        "edad":55,
-        "ubicacion":[2.34567, 3.123],
-        "intereses":["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo","buenos-aires"],
+    "usuario2": {
+        "nombre": "usuario2",
+        "apellido": "usuario2",
+        "contraseña": "usuario2",
+        "sexo": "H",
+        "edad": 55,
+        "ubicacion": [2.34567, 3.123],
+        "intereses": ["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo",
+                      "buenos-aires"],
     },
-	"usuario3":{
-        "nombre":"usuario3",
-        "apellido":"usuario3",
-        "contraseña":"usuario3",
-        "sexo":"M",
-        "edad":75,
-        "ubicacion":[2.4567, 3.123],
-        "intereses":["star-wars","buenos-aires"],
+    "usuario3": {
+        "nombre": "usuario3",
+        "apellido": "usuario3",
+        "contraseña": "usuario3",
+        "sexo": "M",
+        "edad": 75,
+        "ubicacion": [2.4567, 3.123],
+        "intereses": ["star-wars", "buenos-aires"],
     },
-	
+
 }
 
 
-dicBusq={}
+'''
+#para hacer referencia
+usuarios=list(dicDatos.keys())[0]	#ahi me refiero a juan
+
+sexo=dicDatos[usuarios]["sexo"]
+ubicacion=dicDatos[usuarios]["ubicacion"]
+edad=dicDatos[usuarios]["edad"]
+print(edad)	#edad de juan
+'''
+
