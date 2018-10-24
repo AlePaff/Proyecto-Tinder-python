a8a3a8006372d44f9bdd9e018985146487096d47
AlePaff <43119891+AlePaff@users.noreply.github.com>
2018-10-24 14:17:31 -0300

    Update datos_prueba.py

--- datos_prueba.py
+++ datos_prueba.py
@@ -1,27 +1,25 @@
 dicDatos={
-    "Juan89":{
-        nombre:"Juan",
-        apellido:"Rodriguez",
-        contraseña:"juancitoR89",    #contraseña
-        sexo:"Hombre",
-        edad:24,
-        ubicacion:[41.40338,2.123]
-        intereses:["tenis", "nadar", "pokemon", "anime", "escribir", "pintar", "taekwondo"]
+    "Juan89": {
+        "nombre": "Juan",
+        "apellido": "Rodriguez",
+        "contraseña": "juancito",  # contraseña
+        "sexo": "H",
+        "edad": 24,
+        "ubicacion": [41.40338, 2.123],
+        "intereses": ["tenis", "nadar", "pokemon", "anime", "escribir", "pintar", "taekwondo"],
+        "likes": {},
+        "mensajes": {"lasd": 2},
     },
     "LaJuancita":{
-        nombre:"Juana",
-        apellido:"Perez",
-        contraseña:"redsun9887",
-        sexo:"Mujer""",
-        edad:21,
-        ubicacion:[42.34567, 23.123]
-        intereses:["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo","buenos-aires"]
+        "nombre":"Juana",
+        "apellido":"Perez",
+        "contraseña":"redsun9887",
+        "sexo":"M",
+        "edad":21,
+        "ubicacion":[42.34567, 23.123],
+        "intereses":["star-wars", "nueva-york", "fotografia", "francia", "asado", "bicicleta", "taekwondo","buenos-aires"],
     },
 }
 
 
-
 dicBusq={
-	"Juan89":["mujer",[23,32],[41.2,32]]
-	}
-#si te da paja pensar buscá "lista de hobbies"
